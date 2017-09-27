09aa7cdde4a9fd4bba8e7ce4c16df66e37f67268
Jaxon <caozhiqiango@foxmail.com>
2017-09-27 22:40:59 +0800

    创建会员登录页面

--- app/home/views.py
+++ app/home/views.py
@@ -7,10 +7,22 @@
 
 
 from app.home import home
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 
 # 调用蓝图（定义视图）
 @home.route("/")
 def index():
     return render_template("home/index.html")
+
+
+# 定义登录视图
+@home.route("/login/")
+def login():
+    return render_template("home/login.html")
+
+
+# 定义登出视图
+@home.route("/logout/")
+def logout():
+    return redirect(url_for("home.login"))
