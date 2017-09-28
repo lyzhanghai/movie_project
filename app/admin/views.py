858fc7d55801eba012e12e64b549cbf1db2d3a7d
Jaxon <caozhiqiango@foxmail.com>
2017-09-28 17:35:02 +0800

    创建后台登录页面

--- app/admin/views.py
+++ app/admin/views.py
@@ -7,9 +7,22 @@
 
 
 from app.admin import admin
+from flask import render_template, redirect, url_for
 
 
 # 调用蓝图（定义视图）
 @admin.route("/")
 def index():
     return "<h1 style='color:blue'>This is admin</h1>"
+
+
+# 定义登录视图
+@admin.route("/login/")
+def login():
+    return render_template("admin/login.html")
+
+
+# 定义登出视图
+@admin.route("/logout/")
+def logout():
+    return redirect(url_for("admin.login"))
