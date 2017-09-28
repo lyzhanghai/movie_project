7a16cfbe49b56597d464ecbc0ad0f6042aa624c2
Jaxon <caozhiqiango@foxmail.com>
2017-09-28 12:09:19 +0800

    创建会员注册页面和会员中心页面

--- app/home/views.py
+++ app/home/views.py
@@ -26,3 +26,39 @@ def login():
 @home.route("/logout/")
 def logout():
     return redirect(url_for("home.login"))
+
+
+# 定义注册视图
+@home.route("/regist/")
+def regist():
+    return render_template("home/regist.html")
+
+
+# 定义会员中心视图
+@home.route("/user/")
+def user():
+    return render_template("home/user.html")
+
+
+# 定义修改密码视图
+@home.route("/pwd/")
+def pwd():
+    return render_template("home/pwd.html")
+
+
+# 定义评论记录视图
+@home.route("/comments/")
+def comments():
+    return render_template("home/comments.html")
+
+
+# 定义登录日志视图
+@home.route("/loginlog/")
+def loginlog():
+    return render_template("home/loginlog.html")
+
+
+# 定义收藏电影视图
+@home.route("/moviecol/")
+def moviecol():
+    return render_template("home/moviecol.html")
